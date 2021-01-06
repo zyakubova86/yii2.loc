0996264c8235b4771ed40906f0dc56a9fdc19515
Zilola <zilolayakubova.zy@gmail.com>
2021-01-06 18:04:27 +0500

    rendering widget menu

--- components/MenuWidget.php
+++ components/MenuWidget.php
@@ -4,6 +4,7 @@
 namespace app\components;
 
 
+use app\models\Category;
 use yii\base\Widget;
 
 class MenuWidget extends Widget
@@ -29,6 +30,47 @@ class MenuWidget extends Widget
 
     public function run()
     {
-        return $this->tpl;
+        //get cache
+        $menu = \Yii::$app->cache->get('menu');
+        if($menu){
+            return $menu;
+        }
+
+        $this->data = Category::find()->select('id, parent_id, title')->indexBy('id')->asArray()->all();
+        $this->tree = $this->getTree();
+        $this->menuHtml = '<ul class="' . $this->ul_class . '">';
+        $this->menuHtml .= $this->getMenuHtml($this->tree);
+        $this->menuHtml .= '</ul>';
+
+        //set cache
+        \Yii::$app->cache->set('menu', $this->menuHtml, 60);
+        return $this->menuHtml;
+    }
+
+    protected function getTree(){
+        $tree = [];
+        $data = $this->data;
+        foreach ($data as $id=>&$node) {
+            if (!$node['parent_id']){
+                $tree[$id] = &$node;
+            }else{
+                $data[$node['parent_id']]['children'][$node['id']] = &$node;
+            }
+        }
+        return $tree;
+    }
+
+    protected function getMenuHtml($tree){
+        $str = '';
+        foreach($tree as $category){
+            $str .= $this->catToTemplate($category);
+        }
+        return $str;
+    }
+
+    protected function catToTemplate($category){
+        ob_start();
+        include __DIR__ . '/menu_tpl/' . $this->tpl;
+        return ob_get_clean();
     }
 }
--- views/layouts/inc/sidebar.php
+++ views/layouts/inc/sidebar.php
@@ -13,7 +13,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
             <?= \app\components\MenuWidget::widget([
-                    'tpl' => 'select',
+                    'tpl' => 'menu',
                     'ul_class' => 'nav navbar-nav nav_1',
             ]) ?>
             <!--<ul class="nav navbar-nav nav_1">
